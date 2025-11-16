3717bb65215b5971c556959bd4d45edb10cef48f
Nubila Levon <nubila.levon@outlook.com>
2025-11-16 13:07:39 -0800

    added template stubs

--- FinalProject/Source.cpp
+++ FinalProject/Source.cpp
@@ -1,10 +1,53 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
+
+vector<string> read_dictionary();
+bool misspelled_word(string word, vector<string> words);
+void check_file(vector<string> words);
+
+
 int main() {
 
-	cout << "Hello World!" << endl;
+	
 
 	return 0;
+}
+
+/* Reads the dictionary file
+*  @param --none--
+*  @return a vector of word strings from dictionary
+*/
+
+vector<string> read_dictionary()
+{
+
+
+
+}
+
+/* Finds if a word is not in the vector
+*  @param word - the word to search for
+*  @param words - the vector of words to search
+*  @return true if word not found, false if word is in the vector
+*/
+
+bool misspelled_word(string word, vector<string> words)
+{
+
+
+}
+
+/* Reads a file and checks each word for misspelled_word
+*  @param words - the vector of words to search
+*  @return void
+*/
+
+void check_file(vector<string> words)
+{
+
+
 }
